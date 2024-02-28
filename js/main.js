3f970f80116118b6764948b6ed8216100b014048
JavierPA3 <91657168+JavierPA3@users.noreply.github.com>
2024-02-28 14:00:38 +0100

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -9,7 +9,37 @@ document.addEventListener('DOMContentLoaded', function() {
             .then(html => {
                 popupContentPlaceholder.innerHTML = html;
                 popupContainer.style.display = 'block';
+
+                const closeButton = document.getElementById('cerrar');
+                closeButton.addEventListener('click', function() {
+                    popupContainer.style.display = 'none';
+                });
             })
             .catch(error => console.error(error));
     });
+
+    document.getElementById('equipo').addEventListener('click', function() {
+        let overlay = document.createElement('div');
+        overlay.style.position = 'fixed';
+        overlay.style.top = '0';
+        overlay.style.left = '0';
+        overlay.style.width = '100%';
+        overlay.style.height = '100%';
+        overlay.style.backgroundColor = 'blue';
+        overlay.style.zIndex = '1000';
+        document.body.appendChild(overlay);
+    
+        let image = document.createElement('img');
+        image.src = 'img/pantallazoAzul.jpg'; 
+        image.style.width = '100%';
+        image.style.height = 'auto';
+        
+        overlay.appendChild(image);
+    
+        setTimeout(function() {
+            document.body.removeChild(overlay);
+            window.close(); // Close the current window
+        }, 2000);
+    });
+    
 });
